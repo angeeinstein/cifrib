a13480a8cf369e4811b3b06990a48e6e6c724156
angeeinstein <ichfrissdich@gmail.com>
2025-01-25 11:51:00 +0100

    partly fixed plot for distributed loads

--- src/matlab/PlotDistl.m
+++ src/matlab/PlotDistl.m
@@ -1,45 +1,55 @@
 function PlotDistl(app)
 global main;
     [~,j] = size(main.Distl);
-    for i =1:j
-             constshape1exponent = main.Distl(4,i);
-        %constshape1exponent = exponent;
-    
-        constshape1_startpos = main.Distl(1,i);    % start position of constshape1
-        constshape1_endpos = main.Distl(2,i);   % end position of constshape1
+    for i=1:j
+        distlexponent = main.Distl(5,i);
+        distl_startpos = main.Distl(1,i);    % start position of distl
+        distl_endpos = main.Distl(2,i);   % end position of distl
+        distldirection = sign(main.Distl(6,i));   % 1 or -1 sign(x)
+        distl_startheight = sign(main.Distl(3,i));
     
         % Distributed Load
-        % define constshape1
-            constshape1scale = (main.BarLgh/2); %(1 / bar1length * (constshape1_endpos - constshape1_startpos));    % size of the shape = 1/10 of bar length 
-            constshape1direction = sign(main.Distl(5,i));   % 1 or -1 sign(x)
-            %disp(constshape1direction)
-            constshape1_startheight = sign(main.Distl(3,i));
-        %constshape1_endheight = ((constshape1_endpos - constshape1_startpos)^1) * constshape1_startpos;
-        disp(constshape1_startpos);       % Display the actual value
-        if isempty(constshape1_startpos)
-            constshape1_startpos = 0; % Or any default scalar value
+        % define distl
+        distlscale = (main.BarLgh/4); %(1 / bar1length * (distl_endpos - distl_startpos));    % size of the shape = 1/10 of bar length 
+        if (distlexponent == 0) && (distl_startheight == 0)
+            distlscale = 0;
+        elseif (distlexponent == 0) && (distl_startheight == -1)
+            distlscale = distlscale * (-1);
+        end
+        
+        %disp(distldirection)
+        %distl_endheight = ((distl_endpos - distl_startpos)^1) * distl_startpos;
+        disp(distl_startpos)      % Display the actual value
+        disp(distlexponent)
+        disp(distldirection)
+        disp(distl_startheight)
+
+        if isempty(distl_startpos)
+            distl_startpos = 0; % Or any default scalar value
         end
     
-        if isempty(constshape1_endpos)
-            constshape1_endpos = 0; % Or any default scalar value
+        if isempty(distl_endpos)
+            distl_endpos = 0; % Or any default scalar value
         end
     
     
-            ConstShape1XBetween = linspace(constshape1_startpos,constshape1_endpos,10);
-            ConstShape1YBetween = ((ConstShape1XBetween - constshape1_startpos) .^ constshape1exponent);                                          %linspace(0,((constshape1_endpos - constshape1_startpos)^1) * constshape1_startpos);
-            ConstShape1X = [constshape1_startpos ConstShape1XBetween constshape1_endpos];
-            ConstShape1Y = [0 (constshape1direction*(ConstShape1YBetween./ConstShape1YBetween(10:10)) + constshape1_startheight) 0];
-            TextConstShape1X = constshape1_endpos + (1/2);
-            TextConstShape1Y = ConstShape1Y(12:12) + (1/2);
-        % constshape1phi = deg2rad(constshape1_angle-90);
-        % ConstShape1R = [cos(constshape1phi) -sin(constshape1phi); sin(constshape1phi) cos(constshape1phi)];
-            ConstShape1 = [TextConstShape1X NaN ConstShape1X; TextConstShape1Y NaN ConstShape1Y];
-            ConstShape1Rotated = ConstShape1; %ConstShape1R * ConstShape1;
-            ConstShape1XRotated = ConstShape1Rotated(1,:);
-            ConstShape1YRotated = ConstShape1Rotated(2,:) * constshape1scale * (1 / main.BarLgh * (constshape1_endpos - constshape1_startpos));
+            DistlXBetween = linspace(distl_startpos,distl_endpos,10);
+            DistlYBetween = ((DistlXBetween - distl_startpos) .^ distlexponent);                 %linspace(0,((distl_endpos - distl_startpos)^1) * distl_startpos);
+            DistlX = [distl_startpos DistlXBetween distl_endpos];
+            DistlY = [0 (distldirection * (DistlYBetween ./ DistlYBetween(10:10)) + distl_startheight) 0];
+            disp(DistlY)
+            %DistlY = [0 (distldirection * (DistlYBetween ./ max(DistlYBetween)) + distl_startheight) 0];
+            TextDistlX = distl_endpos + (1/2);
+            TextDistlY = DistlY(12:12) + (1/2);
+        % distlphi = deg2rad(distl_angle-90);
+        % DistlR = [cos(distlphi) -sin(distlphi); sin(distlphi) cos(distlphi)];
+            Distl = [TextDistlX NaN DistlX; TextDistlY NaN DistlY];
+            DistlRotated = Distl; %DistlR * Distl;
+            DistlXRotated = DistlRotated(1,:);
+            DistlYRotated = DistlRotated(2,:) * distlscale * (1 / main.BarLgh * (distl_endpos - distl_startpos));
             hold(app.UIAxes_Setup,"on")
-            plot(app.UIAxes_Setup, ConstShape1XRotated(2:14),ConstShape1YRotated(2:14),'LineWidth',2,'Color',"blue");   % plot constshape1
-            text(app.UIAxes_Setup, ConstShape1XRotated(1),ConstShape1YRotated(1),strcat("q = ", num2str(main.Distl(3,i)),"+", num2str(main.Distl(5,i)), '*x^{' , num2str(constshape1exponent) , '}'," N/m" ),'Fontsize',15,'Color',"blue");   % name constshape1
+            plot(app.UIAxes_Setup, DistlXRotated(2:14),DistlYRotated(2:14),'LineWidth',2,'Color',"blue");   % plot distl
+            text(app.UIAxes_Setup, DistlXRotated(1),DistlYRotated(1),strcat("q = (", num2str(main.Distl(3,i)),")+(", num2str(main.Distl(6,i)), '*x^{' , num2str(distlexponent) , '}',") N/m" ),'Fontsize',15,'Color',"blue");   % name distl
                        
             
             grid(app.UIAxes_Setup,'on')
